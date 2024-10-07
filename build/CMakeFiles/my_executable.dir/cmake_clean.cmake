449b4c81d1956d905467e81fdb1a53116f1c6514
ye82 <69617023+ye82@users.noreply.github.com>
2024-10-08 01:09:43 +0800

    添加cmake

---
+++ build/CMakeFiles/my_executable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_executable.dir/src/main.cpp.obj"
+  "CMakeFiles/my_executable.dir/src/main.cpp.obj.d"
+  "libmy_executable.dll.a"
+  "my_executable.exe"
+  "my_executable.exe.manifest"
+  "my_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
